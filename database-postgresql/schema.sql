014b220ee20112a3034240ea957ed80b1796364a
Meng <MengSung@gmail.com>
2018-11-17 08:06:12 -0800

    seed.js is complete

--- database-postgresql/schema.sql
+++ database-postgresql/schema.sql
@@ -7,7 +7,6 @@ CREATE SCHEMA public;
 
 CREATE TABLE trailPhotos (
   photo_id SERIAL PRIMARY KEY,
-  trail_url text,
   trail_id integer,
   user_id integer,
   upload_date timestamptz,
--- database-postgresql/seed.js
+++ database-postgresql/seed.js
@@ -1,34 +1,69 @@
-//helper functions to seed data
-const csvFilePath = './preSeedData.csv';
+// helper functions to seed data
 const { Client } = require('pg');
 const faker = require('faker');
 const request = require('request');
 const rawSeedData = require('./rawSeedData.js');
 
-var queryObject = {};
+// each trail id from 1 - 100 is represented in the trailPhotos database
+// not every user has a photo
 
-request('https://loremflickr.com/620/520/nature', (err, res) => {
-  if(err) throw err;
-  queryObject.photo_url = res.request.uri.href;
+const client = new Client({
+  host: 'localhost',
+  database: 'trailPhotosDB',
+  port: 5432,
 })
 
+client.connect();
+
+// enter number of mock trails and maximum number of photos per trail below
+var numSampleTrails = 100;
+var maxNumPhotosPerTrail = 5;
 
 
-// const client = new Client({
-//   host: 'localhost',
-//   database: 'trailPhotosDB',
-//   port: 5432,
-// })
+// helper functions
+var getRandomIntInclusive = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// client.connect();
+var insertionFactory = (trailId, i) => {
+  return new Promise ((resolve, reject) => {
 
-// client.query('SELECT * from trailPhotos', (err, res) => {
-//   if(err) throw err;
-//   console.log(res.rows[0]);
-//   // client.end();
-// });
+    var requestPromise = new Promise((resolve, reject) => { // async process that grabs random nature photo
+      request('https://loremflickr.com/700/520/nature', (err, res) => {
+        if(err) throw err;
+        resolve(res.request.uri.href);
+      })
+    });
 
-// console.log(rawSeedData);
+    requestPromise.then(value => {
+      var queryObject = {};
+      queryObject.trail_id = trailId;
+      queryObject.user_id = getRandomIntInclusive(1, 100);
+      queryObject.photo_url = value;
+      queryObject.upload_date = faker.date.past();
+      queryObject.caption = faker.lorem.words();
+      queryObject.is_hero_photo = i === 1 ? true : false;
+      var {trail_id, user_id, photo_url, user_id, upload_date, caption, is_hero_photo} = queryObject;
+      var postgreSQLStatement = 'INSERT INTO trailPhotos(trail_id, user_id, upload_date, photo_url, caption, is_hero_photo) VALUES($1, $2, $3, $4, $5, $6)';
+      client.query(postgreSQLStatement, [trail_id, user_id, upload_date, photo_url, caption, is_hero_photo], (err, res) => { // async db table insertion
+        if(err) throw err;
+        console.log('...successfull insertion');
+        resolve();
+      });
+    });
+  })
+}
 
+async function iterateTrailIds(){
+  for(var i = 1; i <= numSampleTrails; i++){
+    var random = getRandomIntInclusive(1, maxNumPhotosPerTrail);
+    console.log('inserting photos for trailid', i);
+    for(var j = 1; j <= random; j++){
+      await insertionFactory(i, j);
+    }
+  }
+  console.log('...mock data insertion complete');
+  client.end();
+}
 
-// INSERT INTO trailPhotos(trail_url, trail_id, user_id, upload_date, photo_url, caption, is_hero_photo)VALUES('www.mytrail.com',3,4,'2016-06-22 19:10:25-07','www.myphotourl.com', 'the best caption ever', true);
+iterateTrailIds();
