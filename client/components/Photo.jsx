d5f2fe0770497c4830168f802a5ba09fcfc40a5c
Meng <MengSung@gmail.com>
2018-12-14 00:39:53 -0800

    linted .jsx files

--- client/components/Photo.jsx
+++ client/components/Photo.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from '../styles/Photo.css';
 
 const Photo = (props) => {
-  return(
+  return (
     <div>
       <div className = {styles.container}>
-      <img className = {styles.image} src = {props.photoItem.attributes.photo_url}></img>
+        <img className = {styles.image} src = {props.photoItem.attributes.photo_url}></img>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Photo;
--- client/components/Photos.jsx
+++ client/components/Photos.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Photo from './Photo.jsx'
+import Photo from './Photo.jsx';
 import styles from '../styles/Photos.css';
 
 const Photos = (props) => {
-  return(
+  return (
     <div className = {styles.container}>
       {props.photos.map(photo => <Photo photoItem = {photo}/>)}
     </div>
-  )
-}
+  );
+};
 
 export default Photos;
