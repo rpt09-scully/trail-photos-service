b78eb1d62ed49e36b7253781c9a55ac54977c4aa
Meng <MengSung@gmail.com>
2018-12-17 18:57:00 -0800

    fixed NaN condition

--- client/services/utilities.js
+++ client/services/utilities.js
@@ -1,6 +1,6 @@
 let getTrailIdURL = () => {
   let urlId = window.location.pathname.slice(1);
-  if (urlId.length === 0 || urlId < 0 || typeof urlId !== Number) {
+  if (urlId.length === 0 || urlId < 0 || isNaN(Number(urlId))) {
     return 1;
   } else {
     return urlId;
