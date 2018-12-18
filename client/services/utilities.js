a1c31df53c2def6007d092ed7f397ce07623c46a
Meng <MengSung@gmail.com>
2018-12-17 18:47:04 -0800

    added handling for non numeric trailIds in the URL

--- client/services/utilities.js
+++ client/services/utilities.js
@@ -1,7 +1,10 @@
 let getTrailIdURL = () => {
   let urlId = window.location.pathname.slice(1);
-  (urlId.length === 0) && (urlId = 1);
-  return urlId;
+  if (urlId.length === 0 || urlId < 0 || typeof urlId !== Number) {
+    return 1;
+  } else {
+    return urlId;
+  }
 };
 
 export default getTrailIdURL;
