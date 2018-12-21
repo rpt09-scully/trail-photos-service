a35acc5ba89bad54f9d0821c56f446619d4d2075
Meng <MengSung@gmail.com>
2018-12-21 08:08:03 -0800

    reset

--- client/index.jsx
+++ client/index.jsx
@@ -16,23 +16,24 @@ class App extends React.Component {
 
   componentDidMount() {
     let SERVICE_HOSTS = {};
-    console.log('PROCESS', process.env);
+    console.log('PROCESS', process.env.NODE_ENV);
     // if (process.env.NODE_ENV === 'production') {
-      SERVICE_HOSTS = {
-        trails: '',
-        profile: '',
-        photos: 'http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com',
-        reviews: '',
-        paths: ''
-      };
+      // SERVICE_HOSTS =
+      // {
+      //   trails: '',
+      //   profile: '',
+      //   photos: 'http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com',
+      //   reviews: '',
+      //   paths: ''
+      // };
     // } else {
-    //   SERVICE_HOSTS = {
-    //     trails: 'http://localhost:3001',
-    //     profile: 'http://localhost:3002',
-    //     photos: 'http://localhost:3003',
-    //     reviews: 'http://localhost:3004',
-    //     paths: 'http://localhost:3005'
-    //   };
+      SERVICE_HOSTS = {
+        trails: 'http://localhost:3001',
+        profile: 'http://localhost:3002',
+        photos: 'http://localhost:3003',
+        reviews: 'http://localhost:3004',
+        paths: 'http://localhost:3005'
+      };
     // }
     let photosEndpoint = SERVICE_HOSTS.photos + `/${this.state.currentTrailId}/photos`;
     // axios.get(`http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com/${this.state.currentTrailId}/photos`)
