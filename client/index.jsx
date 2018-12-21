846c119ed934b9f111acb702dbeba9a90c48b7aa
Meng <MengSung@gmail.com>
2018-12-21 08:30:20 -0800

    take 3

--- client/index.jsx
+++ client/index.jsx
@@ -18,22 +18,22 @@ class App extends React.Component {
     let SERVICE_HOSTS = {};
     console.log('PROCESS', process.env.NODE_ENV);
     // if (process.env.NODE_ENV === 'production') {
-      SERVICE_HOSTS =
-      {
-        trails: '',
-        profile: '',
-        photos: 'http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com',
-        reviews: '',
-        paths: ''
-      };
-    // } else {
-      // SERVICE_HOSTS = {
-      //   trails: 'http://localhost:3001',
-      //   profile: 'http://localhost:3002',
-      //   photos: 'http://localhost:3003',
-      //   reviews: 'http://localhost:3004',
-      //   paths: 'http://localhost:3005'
+      // SERVICE_HOSTS =
+      // {
+      //   trails: '',
+      //   profile: '',
+      //   photos: 'http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com',
+      //   reviews: '',
+      //   paths: ''
       // };
+    // } else {
+      SERVICE_HOSTS = {
+        trails: 'http://localhost:3001',
+        profile: 'http://localhost:3002',
+        photos: 'http://localhost:3003',
+        reviews: 'http://localhost:3004',
+        paths: 'http://localhost:3005'
+      };
     // }
     let photosEndpoint = SERVICE_HOSTS.photos + `/${this.state.currentTrailId}/photos`;
     console.log("endpoint", photosEndpoint);
