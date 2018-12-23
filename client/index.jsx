f29321cdce4d004b67f77d85aa76a8a62b597343
Meng <MengSung@gmail.com>
2018-12-22 17:48:12 -0800

    added to utilities file

--- client/index.jsx
+++ client/index.jsx
@@ -2,39 +2,21 @@ import React from 'react';
 import axios from 'axios';
 import styles from './styles/Index.css';
 import Photos from './components/Photos.jsx';
-import getTrailIdURL from './services/utilities';
+import { getTrailIdURL, detectEnvironment } from './services/utilities';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentTrailId: getTrailIdURL(),
+      environment: detectEnvironment(),
       photos: []
     };
   }
 
   componentDidMount() {
-    let SERVICE_HOSTS = {};
-    if (process.env.NODE_ENV === 'production') {
-      SERVICE_HOSTS =
-      {
-        trails: '',
-        profile: '',
-        photos: 'http://trail-photos-service-dev.us-west-1.elasticbeanstalk.com',
-        reviews: '',
-        paths: ''
-      };
-    } else {
-      SERVICE_HOSTS = {
-        trails: 'http://localhost:3001',
-        profile: 'http://localhost:3002',
-        photos: 'http://localhost:3003',
-        reviews: 'http://localhost:3004',
-        paths: 'http://localhost:3005'
-      };
-    }
-    let photosEndpoint = SERVICE_HOSTS.photos + `/${this.state.currentTrailId}/photos`;
-
+    let photosEndpoint = this.state.environment.photos + `/${this.state.currentTrailId}/photos`;
+    console.log("Meng", photosEndpoint);
     axios.get(photosEndpoint)
       .then((response) => {
         let photoData = response.data.data;
