d6e0b9605fd7a812a449a88bc2e116d1261863f6
Meng <MengSung@gmail.com>
2019-01-15 14:25:13 -0800

    new

--- client/components/PhotoPopUp.jsx
+++ client/components/PhotoPopUp.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/PhotoPopUp.css';
-<<<<<<< HEAD
 import moment from 'moment';
 
 const PhotoPopUp = (props) => {
@@ -32,30 +31,6 @@ const PhotoPopUp = (props) => {
           </div>
         </div>
       </div>
-=======
-
-const PhotoPopUp = (props) => {
-  return (
-    <div className = {styles.container} onClick = { e => props.photoClickTransHandler(e.target)}>
-      <div className = {styles.topContainer}>
-        <div className = {styles.cancel}>X</div>
-        <div className = {styles.prev}>PREV</div>
-        <img className = {styles.image} src = {props.currentPhotoInfo.attributes.photo_url}></img>
-        <div className = {styles.next}>NEXT</div>
-      </div>
-
-      <div className = {styles.bottomContainer}>
-        <img className = {styles.profileImage} src = {props.currentProfileInfo.attributes.photo_url}></img>
-        <div className = {styles.descName}>
-          {props.currentProfileInfo.attributes.first_name} {props.currentProfileInfo.attributes.last_name}<br/>
-          {props.currentPhotoInfo.attributes.caption} <br />
-        </div>
-        <div className = {styles.date}>
-          {props.currentPhotoInfo.attributes.upload_date}
-        </div>
-      </div>
-
->>>>>>> master
     </div>
   );
 };
