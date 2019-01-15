e025b373cb5ff13eac7859cca46e6071038def37
Meng <MengSung@gmail.com>
2019-01-15 14:04:21 -0800

    fixed modal css

--- client/components/PhotoPopUp.jsx
+++ client/components/PhotoPopUp.jsx
@@ -3,20 +3,25 @@ import styles from '../styles/PhotoPopUp.css';
 import moment from 'moment';
 
 const PhotoPopUp = (props) => {
+
   props.currentPhotoInfo.attributes.rel_upload_date = moment(props.currentPhotoInfo.attributes.upload_date).fromNow();
 
   return (
     <div className = {styles.master}>
       <div className = {styles.container} onClick = { e => props.photoClickTransHandler(e.target)}>
         <div className = {styles.topContainer}>
-          <div className = {styles.cancel}>&times;</div>
-          <div className = {styles.prev}>PREV</div>
+          <div className = {styles.prev}>
+            <div className = {styles.prevItem}>PREV</div>
+          </div>
           <img className = {styles.image} src = {props.currentPhotoInfo.attributes.photo_url}></img>
-          <div className = {styles.next}>NEXT</div>
+          <div className = {styles.next}>
+            <div className = {styles.cancel}>&times;</div>
+            <div className = {styles.nextItem}>NEXT</div>
+          </div>
         </div>
 
         <div className = {styles.bottomContainer}>
-          <img className = {styles.profileImage} src = {props.currentProfileInfo.attributes.photo_url} onError={(e) => e.target.src = 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif'} ></img>
+          <img className = {styles.profileImage} src = {props.currentProfileInfo.attributes.photo_url} onError={e => e.target.src = 'https://static.xx.fbcdn.net/rsrc.php/v3/yo/r/UlIqmHJn-SK.gif'} ></img>
           <div className = {styles.descName}>
             {props.currentProfileInfo.attributes.first_name} {props.currentProfileInfo.attributes.last_name}<br/>
             {props.currentPhotoInfo.attributes.caption} <br />
