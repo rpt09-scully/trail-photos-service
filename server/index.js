15bfd0e786390d7217e4ca0cc587cbcd27d23f59
MengRS <MengSung@gmail.com>
2019-02-14 09:03:24 -0800

    03 client and test (#22)
    * minor edits to readme.md
    
    * added /{trailid} route
    
    * added path to index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3003;
+const path = require('path');
 
 const trailPhotos = require('./trailPhotos');
 
